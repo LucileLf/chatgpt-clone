14732b8922900e6fbcc988840fd7c6cf37e758ad
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-07-31 21:39:54 +0200

    chat page design

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,8 +1,49 @@
-import './chatPage.css'
+import './chatPage.scss'
+import NewPrompt from '../../components/newPrompt/NewPrompt.jsx'
 
 const ChatPage = () => {
+
   return (
-    <div className='chatPage'>ChatPage</div>
+    <div className='chatPage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt/>
+          {/* <div ref={endRef} /> */}
+        </div>
+      </div>
+    </div>
   )
 }
 
